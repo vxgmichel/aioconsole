18c86dc2d9c85de7c929f2eea2594c4e19350195
vinmic <vincent.michel@maxlab.lu.se>
2015-10-20 21:59:32 +0200

    add async syntax for python 3.5

--- apython/execute.py
+++ apython/execute.py
@@ -1,9 +1,16 @@
 """Provide an asynchronous equivalent *to exec*."""
 
 import ast
+import codeop
 import asyncio
 
+from . import compat
+
 CORO_NAME = '__corofn'
+CORO_DEF = 'def {}(): '.format(CORO_NAME)
+if compat.PY35:
+    CORO_DEF = 'async ' + CORO_DEF
+CORO_CODE = CORO_DEF + 'return (None, locals())\n'
 
 
 def full_update(dct, values):
@@ -18,19 +25,18 @@ def exec_result(obj, filename="<aexec>", symbol="single"):
     exec(code, {'result': obj})
 
 
-def make_coroutine(statement, filename="<aexec>", symbol="single", local={}):
+def make_tree(statement, filename="<aexec>", symbol="single", local={}):
     """Helper for *aexec*."""
     # Create tree
-    base_code = "def {}(): return (None, locals())\n".format(CORO_NAME)
-    tree = ast.parse(base_code, filename, symbol)
+    tree = ast.parse(CORO_CODE, filename, symbol)
     # Check expression statement
     if isinstance(statement, ast.Expr):
         tree.body[0].body[0].value.elts[0] = statement.value
     else:
         tree.body[0].body.insert(0, statement)
-    # Create coroutine
-    coro = make_coroutine_from_tree(tree, filename, symbol, local)
-    return coro, tree
+    # Check and return coroutine
+    exec(compile(tree, filename, symbol))
+    return tree
 
 
 def make_coroutine_from_tree(tree, filename="<aexec>", symbol="single",
@@ -42,10 +48,27 @@ def make_coroutine_from_tree(tree, filename="<aexec>", symbol="single",
     return asyncio.coroutine(dct[CORO_NAME])(**local)
 
 
-def compile_for_aexec(source, filename="<aexec>", symbol="single", local={}):
+def compile_for_aexec(source, filename="<aexec>", mode="single",
+                      dont_imply_dedent=False, local={}):
     """Return a list of (coroutine object, abstract base tree)."""
-    interactive = ast.parse(source, filename, symbol)
-    return [make_coroutine(statement, filename, symbol) for
+    flags = ast.PyCF_ONLY_AST
+    if dont_imply_dedent:
+        flags |= codeop.PyCF_DONT_IMPLY_DEDENT
+    if compat.PY35:
+        # Avoid a syntax error by wrapping code with `async def`
+        indented = '\n'.join(line and ' ' * 4 + line
+                             for line in source.split('\n'))
+        coroutine = CORO_DEF + '\n' + indented + '\n'
+        interactive = compile(coroutine, filename, mode, flags).body[0]
+        # Check EOF errors
+        try:
+            compile(source, filename, mode, flags)
+        except SyntaxError as exc:
+            if exc.msg == 'unexpected EOF while parsing':
+                raise
+    else:
+        interactive = compile(source, filename, mode, flags)
+    return [make_tree(statement, filename, mode) for
             statement in interactive.body]
 
 
@@ -59,7 +82,7 @@ def aexec(source, local=None):
         local = {}
     if isinstance(source, str):
         source = compile_for_aexec(source)
-    for _, tree in source:
+    for tree in source:
         coro = make_coroutine_from_tree(tree, local=local)
         result, new_local = yield from coro
         exec_result(result)
