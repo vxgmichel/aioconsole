f992e88c3d4a610d37bbc2b1825874aafe569ba8
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-11-11 18:32:51 +0100

    update example

--- example/cli.py
+++ example/cli.py
@@ -3,13 +3,12 @@
 import fnmatch
 import asyncio
 import argparse
-from apython import AsynchronousCli
-from .echo import main
+import apython
+from . import echo
 
 
 @asyncio.coroutine
-def get_history(pattern=None):
-    result = ''
+def get_history(reader, writer, pattern=None):
     history = asyncio.get_event_loop().history
     if not history:
         return "No message in the history"
@@ -19,22 +18,48 @@ def get_history(pattern=None):
     if not history:
         return "No host match the given pattern"
     for host in history:
-        result += 'Host {}:\n'.format(host)
+        writer.write('Host {}:\n'.format(host).encode())
         for i, message in enumerate(history[host]):
-            result += '  {}. {}\n'.format(i, message)
-    return result[:-1]
+            writer.write('  {}. {}\n'.format(i, message).encode())
 
 
-def schedule_cli():
+def make_cli(streams=None):
     parser = argparse.ArgumentParser(description="Display the message history")
     parser.add_argument('--pattern', '-p', type=str,
                         help='pattern to filter hostnames')
     commands = {'history': (get_history, parser)}
-    cli = AsynchronousCli(commands, prog='echo')
-    asyncio.async(cli.interact())
+    return apython.AsynchronousCli(commands, streams, prog='echo')
 
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description="Run the echo server and a command line interface.")
+    parser.add_argument(
+        '--port',
+        '-p',
+        type=int,
+        default=8000,
+        help='port for the echo server, default is 8000')
+    parser.add_argument(
+        '--serve-cli',
+        metavar='PORT',
+        type=int,
+        help='serve the command line interface on the given port '
+        'instead of using the standard streams')
+    namespace = parser.parse_args(args)
+    return namespace.port, namespace.serve_cli
+
+
+def main(args=None):
+    port, serve_cli = parse_args(args)
+    if not serve_cli:
+        asyncio.async(make_cli().interact())
+    else:
+        coro = apython.start_interactive_server(make_cli, '', serve_cli)
+        asyncio.get_event_loop().run_until_complete(coro)
+        msg = 'A command line interface is being served on port {}'
+        print(msg.format(serve_cli))
+    return echo.main(port)
 
 if __name__ == '__main__':
-    import sys
-    schedule_cli()
-    main(*sys.argv[1:])
+    main()
