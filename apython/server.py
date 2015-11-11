a41c1393ecb160d77d5d7eeb4e2386ec22bb2387
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-11-11 18:30:26 +0100

    add start_interactive_server

--- apython/server.py
+++ apython/server.py
@@ -3,15 +3,22 @@ from . import code
 
 
 @asyncio.coroutine
-def handle_connect(reader, writer):
+def handle_connect(reader, writer, factory):
     streams = reader, writer
-    yield from code.interact(streams=streams, stop=False, handle_sigint=False)
+    cli = factory(streams=streams)
+    yield from cli.interact(stop=False, handle_sigint=False)
     writer.close()
+
+
+@asyncio.coroutine
+def start_interactive_server(factory, host='', port=8000, *, loop=None):
+    callback = lambda reader, writer: handle_connect(reader, writer, factory)
+    yield from asyncio.start_server(callback, host, port, loop=loop)
 
 
 def main(port=8000):
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handle_connect, '', port)
+    coro = start_interactive_server(code.AsynchronousConsole, '', port)
     loop.server = loop.run_until_complete(coro)
     try:
         loop.run_forever()
