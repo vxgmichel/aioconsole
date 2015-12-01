0cf1da2d7474cf23acf31dd3b8d23737930171f7
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-12-01 13:06:15 +0100

    add argument parsing for apython.server

--- apython/server.py
+++ apython/server.py
@@ -1,4 +1,7 @@
+"""Serve the python console using socket communication."""
+
 import asyncio
+import argparse
 from . import code
 
 
@@ -16,16 +19,32 @@ def start_interactive_server(factory, host='', port=8000, *, loop=None):
     yield from asyncio.start_server(callback, host, port, loop=loop)
 
 
-def main(port=8000):
+def run(port=8000):
     loop = asyncio.get_event_loop()
     coro = start_interactive_server(code.AsynchronousConsole, '', port)
     loop.server = loop.run_until_complete(coro)
+    print('The python console is being served on port {} ...'.format(port))
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         pass
 
 
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description="Serve the python console.")
+    parser.add_argument(
+        'port',
+        metavar='PORT',
+        type=int,
+        default=8000,
+        help='default port is 8000')
+    namespace = parser.parse_args(args)
+    return namespace.port
+
+
+def main(args=None):
+    run(parse_args(args))
+
 if __name__ == '__main__':
-    import sys
-    main(*sys.argv[1:])
+    main()
