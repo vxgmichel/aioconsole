c8a40150c05dffb303dbd7bdd3ef2a87cf8f9347
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-12-03 17:39:33 +0100

    make AsynchronousConsole accessible at top-level

--- apython/__init__.py
+++ apython/__init__.py
@@ -5,14 +5,14 @@ It also includes an interactive event loop, and a command line interface.
 
 from .execute import aexec
 from .stream import ainput
-from .code import interact
+from .code import AsynchronousConsole, interact
 from .events import InteractiveEventLoop, InteractiveEventLoopPolicy
 from .events import set_interactive_policy, run_console
 from .command import AsynchronousCli
 from .server import start_interactive_server
 from .cli import main
 
-__all__ = ['aexec', 'ainput', 'interact',
+__all__ = ['aexec', 'ainput', 'AsynchronousConsole', 'interact',
            'InteractiveEventLoop', 'InteractiveEventLoopPolicy',
            'set_interactive_policy', 'run_console',
            'AsynchronousCli', 'start_interactive_server',
