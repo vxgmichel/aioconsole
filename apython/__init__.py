560f91277f4028d988ae403b009b4b54420353f6
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-02-22 11:00:52 +0100

    Expose get_standard_streams at top level

--- apython/__init__.py
+++ apython/__init__.py
@@ -4,8 +4,8 @@ It also includes an interactive event loop, and a command line interface.
 """
 
 from .execute import aexec
-from .stream import ainput
 from .code import AsynchronousConsole, interact
+from .stream import ainput, get_standard_streams
 from .events import InteractiveEventLoop, InteractiveEventLoopPolicy
 from .events import set_interactive_policy, run_console
 from .command import AsynchronousCli
@@ -16,4 +16,4 @@ __all__ = ['aexec', 'ainput', 'AsynchronousConsole', 'interact',
            'InteractiveEventLoop', 'InteractiveEventLoopPolicy',
            'set_interactive_policy', 'run_console',
            'AsynchronousCli', 'start_interactive_server',
-           'main']
+           'get_standard_streams', 'main']
