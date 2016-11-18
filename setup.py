cd59b3ceb1d99fa6a07b6270cc8cebd5ee4f0932
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-11-18 11:52:42 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ CLASSIFIERS = """\
 Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 """.splitlines()
 
 setup(
