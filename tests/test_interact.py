473d55dffa7aeb1bc12fa0f64e88ec2f36eb4e0b
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-12-20 17:26:09 +0100

    Fix tests for python3.4

--- tests/test_interact.py
+++ tests/test_interact.py
@@ -4,6 +4,7 @@ import sys
 
 import pytest
 from aioconsole import interact
+from aioconsole import compat
 
 
 @pytest.mark.asyncio
@@ -23,10 +24,10 @@ def test_interact_traceback(event_loop):
     sys.stdin = io.StringIO('1/0\n')
     sys.stderr = io.StringIO()
     banner = "A BANNER"
-    expected = '\n'.join((banner, sys.ps1 + '2', sys.ps1)) + '\n'
     yield from interact(banner=banner, stop=False)
     lines = sys.stderr.getvalue().splitlines()
     assert lines[0] == banner
     assert lines[1] == sys.ps1 + "Traceback (most recent call last):"
-    assert lines[5] == "ZeroDivisionError: division by zero"
-    assert lines[6] == sys.ps1
+    index = 5 if compat.PY35 else 7
+    assert lines[index] == "ZeroDivisionError: division by zero"
+    assert lines[index+1] == sys.ps1
