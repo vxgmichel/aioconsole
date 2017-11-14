a4e6e5b780c8a8d2eda4037ff565a4e49c6ce52a
Vincent Michel <vincent.michel@esrf.fr>
2017-11-14 15:51:43 +0100

    Update apython argument parsing

--- aioconsole/apython.py
+++ aioconsole/apython.py
@@ -1,3 +1,4 @@
+"""Provide the apython script."""
 
 import os
 import sys
@@ -10,23 +11,29 @@ import subprocess
 from . import events
 from . import server
 
-
-def parse_args(args):
-    parser = argparse.ArgumentParser(prog='apython', description='''\
+DESCRIPTION = """\
 Run the given python file or module with a modified asyncio policy replacing
 the default event loop with an interactive loop.
-If no argument is given, it simply runs an asynchronous python console.
-''')
-    parser.add_argument('--serve', '-s', metavar='[HOST:] PORT',
-                        help='serve a console on the given interface instead')
-    parser.add_argument('--module', '-m', dest='module', action='store_true',
-                        help='run a python module')
-    parser.add_argument('--no-readline', dest='readline',
-                        action='store_false', help='Force readline disabling')
-    parser.add_argument('filename', metavar='FILE', nargs='?',
-                        help='python file or module to run')
-    parser.add_argument('args', metavar='ARGS', nargs=argparse.REMAINDER,
-                        help='extra arguments')
+If no argument is given, it simply runs an asynchronous python console."""
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(prog='apython', description=DESCRIPTION)
+    parser.add_argument(
+        '--serve', '-s', metavar='[HOST:] PORT',
+        help='serve a console on the given interface instead')
+    parser.add_argument(
+        '--module', '-m', dest='module', action='store_true',
+        help='run a python module')
+    parser.add_argument(
+        '--no-readline', dest='readline', action='store_false',
+        help='Force readline disabling')
+    parser.add_argument(
+        'filename', metavar='FILE', nargs='?',
+        help='python file or module to run')
+    parser.add_argument(
+        'args', metavar='ARGS', nargs=argparse.REMAINDER,
+        help='extra arguments')
     namespace = parser.parse_args(args)
     if namespace.module and not namespace.filename:
         parser.error('A python module is required.')
@@ -36,8 +43,6 @@ If no argument is given, it simply runs an asynchronous python console.
 
 
 def run_apython(args=None):
-    if args is None:
-        args = sys.argv[1:]
     namespace = parse_args(args)
 
     try:
