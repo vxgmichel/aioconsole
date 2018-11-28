c17b8c733c7972d2a70de1d13c4be8a2e071d703
Vincent Michel <vincent.michel@esrf.fr>
2018-11-28 17:04:10 +0100

    Bump version 0.1.12.dev0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ Programming Language :: Python :: 3.7
 
 setup(
     name="aioconsole",
-    version="0.1.11",
+    version="0.1.12.dev0",
     packages=["aioconsole"],
     entry_points={'console_scripts': ['apython = aioconsole:run_apython']},
 
