23ab732b1ac119091f7d0f8c43e101cbbc0aab59
Vincent Michel <vxgmichel@gmail.com>
2020-06-10 19:05:34 +0200

    Add pre-commit configuration file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import sys
 from setuptools import setup
 
-TESTING = any(x in sys.argv for x in ['test', 'pytest'])
+TESTING = any(x in sys.argv for x in ["test", "pytest"])
 
 README = open("README.rst").read()
 
@@ -20,21 +20,15 @@ setup(
     name="aioconsole",
     version="0.1.17.dev0",
     packages=["aioconsole"],
-    entry_points={'console_scripts': ['apython = aioconsole:run_apython']},
-
-    setup_requires=['pytest-runner' if TESTING else ''],
-    tests_require=['pytest',
-                   'pytest-asyncio>=0.8',
-                   'pytest-cov',
-                   'pytest-repeat'],
-
+    entry_points={"console_scripts": ["apython = aioconsole:run_apython"]},
+    setup_requires=["pytest-runner" if TESTING else ""],
+    tests_require=["pytest", "pytest-asyncio>=0.8", "pytest-cov", "pytest-repeat"],
     license="GPLv3",
     classifiers=CLASSIFIERS,
     description="Asynchronous console and interfaces for asyncio",
     long_description=README,
-
     author="Vincent Michel",
     author_email="vxgmichel@gmail.com",
-    url='https://github.com/vxgmichel/aioconsole',
-    download_url='http://pypi.python.org/pypi/aioconsole',
+    url="https://github.com/vxgmichel/aioconsole",
+    download_url="http://pypi.python.org/pypi/aioconsole",
 )
