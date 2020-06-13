f3fa3d5093763f6cc96ec07a5c9d546f96229915
Vincent Michel <vxgmichel@gmail.com>
2020-06-13 11:58:59 +0200

    Bump version 0.2.2.dev0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ Programming Language :: Python :: 3.7
 
 setup(
     name="aioconsole",
-    version="0.2.1",
+    version="0.2.2.dev0",
     packages=["aioconsole"],
     entry_points={"console_scripts": ["apython = aioconsole:run_apython"]},
     setup_requires=["pytest-runner" if TESTING else ""],
