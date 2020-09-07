9f4027fb09135a8d819b6ac4eabe963a5d51ef3d
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-09-07 17:51:17 +0300

    Format with Black

--- docs/conf.py
+++ docs/conf.py
@@ -2,26 +2,26 @@
 
 import sphinx_rtd_theme
 
-VERSION = open('../setup.py').read().split("version=\"")[1].split("\"")[0]
+VERSION = open("../setup.py").read().split('version="')[1].split('"')[0]
 
-project = 'aioconsole'
+project = "aioconsole"
 version = VERSION
-author = 'Vincent Michel'
-copyright = u'2020, Vincent Michel'
+author = "Vincent Michel"
+copyright = u"2020, Vincent Michel"
 
-master_doc = 'index'
-highlight_language = 'python'
-extensions = ['sphinx.ext.autodoc']
+master_doc = "index"
+highlight_language = "python"
+extensions = ["sphinx.ext.autodoc"]
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_context = {
-    'display_github': True,
-    'github_user': 'vxgmichel',
-    'github_repo': 'aioconsole',
-    'github_version': "master",
-    'conf_py_path': "/docs/",
-    'source_suffix': '.rst',
+    "display_github": True,
+    "github_user": "vxgmichel",
+    "github_repo": "aioconsole",
+    "github_version": "master",
+    "conf_py_path": "/docs/",
+    "source_suffix": ".rst",
 }
 
-suppress_warnings = ['image.nonlocal_uri']
+suppress_warnings = ["image.nonlocal_uri"]
