a06836173c3dabfb05075583b32b4ef27b043f89
Artur Svistunov <18216480+madbird1304@users.noreply.github.com>
2021-04-13 14:36:15 +0300

    Update default: `aprint(flush=False)` for `aprint(flush=True)`
    Before the update, `writer.drain()` was being called implicitly
    despite of value passed with `flush` parameter.
    It was reasonable in most of cases, so now `flush=True` by default

--- aioconsole/stream.py
+++ aioconsole/stream.py
@@ -210,7 +210,7 @@ async def ainput(prompt="", *, streams=None, use_stderr=False, loop=None):
 
 
 async def aprint(
-    *values, sep=None, end="\n", flush=False, streams=None, use_stderr=False, loop=None
+    *values, sep=None, end="\n", flush=True, streams=None, use_stderr=False, loop=None
 ):
     """Asynchronous equivalent to *print*."""
     # Get standard streams
