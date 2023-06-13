a653fc9d023dcc7a1e57f5ea5cc05ea4b0fcf699
Vincent Michel <vxgmichel@gmail.com>
2023-06-13 10:52:45 +0200

    Update echo server example

--- example/echo.py
+++ example/echo.py
@@ -1,40 +1,42 @@
+import sys
 import asyncio
 import collections
 
 
-async def handle_echo(reader, writer):
+async def handle_echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     loop = asyncio.get_event_loop()
-    data = await reader.read(100)
+    data = await reader.readline()
     message = data.decode()
     addr = writer.get_extra_info("peername")
     loop.history[addr[0]].append(message.strip())
     writer.write(data)
     await writer.drain()
     writer.close()
+    await writer.wait_closed()
 
 
-def run(host="localhost", port=8000):
+async def run(host: str = "localhost", port: int = 8000):
     loop = asyncio.get_event_loop()
     loop.history = collections.defaultdict(list)
-    coro = asyncio.start_server(handle_echo, host, port, loop=loop)
-    loop.server = loop.run_until_complete(coro)
+    loop.server = await asyncio.start_server(handle_echo, host, port)
     interface = "{}:{}".format(*loop.server.sockets[0].getsockname())
     print(f"The echo service is being served on {interface}")
+    async with loop.server:
+        await loop.server.serve_forever()
+
+
+def main(args: list[str]):
+    (server,) = args[1:]
+    if ":" not in server:
+        coro = run(port=int(server))
+    else:
+        host, port = server.split(":")
+        coro = run(host, int(port))
     try:
-        loop.run_forever()
+        asyncio.run(coro)
     except KeyboardInterrupt:
         pass
 
 
-def main(args):
-    (server,) = args[1:]
-    if ":" not in server:
-        return run(port=int(server))
-    host, port = server.split(":")
-    return run(host, int(port))
-
-
 if __name__ == "__main__":
-    import sys
-
     main(sys.argv)
