e2c633c09172c2cdd87ce8c020b754f35219cd59
Peter Bábics <peter.ntx@gmail.com>
2023-10-24 16:35:57 +0200

    feat: add option for persiting updated locals between connections

--- tests/test_server.py
+++ tests/test_server.py
@@ -30,6 +30,42 @@ async def test_server(event_loop):
 
 
 @pytest.mark.asyncio
+async def test_server_shared_locals(event_loop):
+    server = await start_console_server(
+        host="127.0.0.1", port=0, banner="test", shared_locals=True, locals={}
+    )
+    address = server.sockets[0].getsockname()
+
+    stream = io.StringIO()
+    print_server(server, "test console", file=stream)
+    expected = f"The test console is being served on 127.0.0.1:{address[1]}\n"
+    assert stream.getvalue() == expected
+
+    reader, writer = await asyncio.open_connection(*address)
+    assert (await reader.readline()) == b"test\n"
+    writer.write(b"value=1+1\n")
+    assert (await reader.read(5)) == b">>> "
+    writer.write(b"value\n")
+    assert (await reader.readline()) == b">>> 2\n"
+    writer.write_eof()
+    assert (await reader.readline()) == b">>> \n"
+    writer.close()
+    await writer.wait_closed()
+
+    reader, writer = await asyncio.open_connection(*address)
+    assert (await reader.readline()) == b"test\n"
+    writer.write(b"value\n")
+    assert (await reader.readline()) == b">>> 2\n"
+    writer.write_eof()
+    assert (await reader.readline()) == b">>> \n"
+    writer.close()
+    await writer.wait_closed()
+
+    server.close()
+    await server.wait_closed()
+
+
+@pytest.mark.asyncio
 async def test_uds_server(event_loop, tmpdir_factory):
     path = str(tmpdir_factory.mktemp("uds") / "my_uds")
 
